be7f22703b89c0f8e4e8f00cd000e50888fbe1c2
Artem Romanov <>
2023-04-26 12:20:07 +0400

    init

---
+++ demo_mvvm/demo_mvvm/demo_mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_mvvmApp.swift
+//  Demo_mvvm
+//
+//  Created by Артем Романов on 26.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Demo_mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
